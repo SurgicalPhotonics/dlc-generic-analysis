23e473d5f343264ddb87eca4457f40020dc321ac
Louis Adamian <louis@adamian.net>
2021-11-22 17:14:16 -0500

    adds video view to trimmer

--- trimmer_gui.py
+++ trimmer_gui.py
@@ -1,6 +1,5 @@
 import platform
 import sys
-
 from PySide2 import QtWidgets, QtCore, QtMultimedia, QtMultimediaWidgets
 from typing import List
 
@@ -19,6 +18,7 @@ class GoToTime(QtWidgets.QWidget):
 class TrimWidget(QtWidgets.QWidget):
     def __init__(self, video_paths: List[str], parent=None):
         super(TrimWidget, self).__init__()
+        self.setWindowTitle("Trim Video(s)")
         if platform.system() == "Windows":
             fast_reverse = "\u23EA"
             fast_forward = "\u23E9"
@@ -37,35 +37,37 @@ class TrimWidget(QtWidgets.QWidget):
         self.top_ui = QtWidgets.QWidget()
         self.top_ui.setLayout(QtWidgets.QHBoxLayout())
         self.top_ui.videos_navigate = QtWidgets.QWidget()
-        self.videos_navigate = QtWidgets.QWidget()
-        self.videos_navigate.setLayout(QtWidgets.QHBoxLayout())
-        self.previous_video_button = QtWidgets.QPushButton("Previous Video")
-        self.previous_video_button.clicked.connect(self.on_click_previous_video)
-        self.previous_video_button.setDisabled(True)
-        self.videos_navigate.layout().addWidget(self.previous_video_button)
-        self.next_video_button = QtWidgets.QPushButton("Next Video")
-        self.next_video_button.clicked.connect(self.on_click_next_video)
-        self.videos_navigate.layout().addWidget(self.next_video_button)
-        self.top_ui.layout().addWidget(self.videos_navigate)
-        self.top_ui.filename_label = QtWidgets.QLabel(self.video_paths[0])
+        if len(video_paths) > 1:
+            self.videos_navigate = QtWidgets.QWidget()
+            self.videos_navigate.setLayout(QtWidgets.QHBoxLayout())
+            self.previous_video_button = QtWidgets.QPushButton("Previous Video")
+            self.previous_video_button.clicked.connect(self.on_click_previous_video)
+            self.previous_video_button.setDisabled(True)
+            self.videos_navigate.layout().addWidget(self.previous_video_button)
+            self.next_video_button = QtWidgets.QPushButton("Next Video")
+            self.next_video_button.clicked.connect(self.on_click_next_video)
+            self.videos_navigate.layout().addWidget(self.next_video_button)
+            self.top_ui.layout().addWidget(self.videos_navigate)
+        self.top_ui.filename_label = QtWidgets.QLabel("")
 
         self.video_viewer = QtMultimediaWidgets.QVideoWidget()
         self.player = QtMultimedia.QMediaPlayer()
-        self.player.setVideoOutput(self.video_viewer)
         self.player.setMuted(True)
 
-        self.load_video(self.video_paths[0])
         self.trim_beginning = 0  # the beginning point for the trim in milliseconds
         self.trim_end = self.player.duration()  # the end point for the trim in milliseconds
         self.play_pause_widget = QtWidgets.QWidget()
         self.play_pause_widget.setLayout(QtWidgets.QHBoxLayout())
-        self.fast_reverse_button = QtWidgets.QPushButton(fast_reverse)
+        self.fast_reverse_button = QtWidgets.QPushButton()
+        self.fast_reverse_button.setIcon(self.style().standardIcon(QtWidgets.QStyle.SP_MediaSeekForward))
         self.fast_reverse_button.clicked.connect(self.on_click_fast_reverse)
         self.play_pause_widget.layout().addWidget(self.fast_reverse_button)
-        self.play_pause_button = QtWidgets.QPushButton(play_pause)
+        self.play_pause_button = QtWidgets.QPushButton()
+        self.play_pause_button.setIcon(self.style().standardIcon(QtWidgets.QStyle.SP_MediaPlay))
         self.play_pause_button.clicked.connect(self.on_click_play_pause)
         self.play_pause_widget.layout().addWidget(self.play_pause_button)
-        self.fast_forward_button = QtWidgets.QPushButton(fast_forward)
+        self.fast_forward_button = QtWidgets.QPushButton()
+        self.fast_reverse_button.setIcon(self.style().standardIcon(QtWidgets.QStyle.SP_MediaSeekBackward))
         self.fast_forward_button.clicked.connect(self.on_click_fast_forward)
         self.play_pause_widget.layout().addWidget(self.fast_forward_button)
 
@@ -84,6 +86,8 @@ class TrimWidget(QtWidgets.QWidget):
         self.trim_control.setLayout(QtWidgets.QGridLayout())
         self.trim_control.layout().addWidget(self.play_pause_widget)
         self.slider = QtWidgets.QSlider(QtCore.Qt.Orientation.Horizontal)
+        self.slider.setRange(0, 0)
+        self.slider.sliderMoved.connect(self.on_slider_move)
         self.trim_control.layout().addWidget(self.slider)
         self.trim_control.layout().addWidget(self.trim_buttons_widget)
         self.trim_control.layout().addWidget(self.goto_start)
@@ -92,31 +96,35 @@ class TrimWidget(QtWidgets.QWidget):
         self.layout().addWidget(self.top_ui)
         self.layout().addWidget(self.video_viewer)
         self.layout().addWidget(self.trim_control)
-        # self.loop = QtCore.QEventLoop(self)
-        # self.loop.processEvents(self.e)
-        self.show()
-        # self.loop.exec_()
+
+        self.player.stateChanged.connect(self.state_changed)
+        self.player.positionChanged.connect(self.position_changed)
+        self.player.durationChanged.connect(self.duration_changed)
+        self.player.setVideoOutput(self.video_viewer)
+        self.load_video(self.video_paths[0])
 
     def load_video(self, path):
-        self.player.setMedia(QtCore.QUrl.fromLocalFile(path))
+        self.player.setMedia(QtMultimedia.QMediaContent(QtCore.QUrl.fromLocalFile(path)))
 
     def on_click_play_pause(self, e):
         state = self.player.state()
         if state == QtMultimedia.QMediaPlayer.PlayingState:
             self.player.pause()
+            print("pause")
         elif state == QtMultimedia.QMediaPlayer.PausedState:
             self.player.play()
-        elif state == QtMultimedia.QMediaPlayer.StoppedState:
-            pass
-            # play from beginning
+            print("play")
 
     def on_click_fast_forward(self, e):
-        if self.player.playbackState() != QtMultimedia.QMediaPlayer.StoppedState:
+        if self.player.state() != QtMultimedia.QMediaPlayer.StoppedState:
             self.player.setPosition(self.player.position()+1000)
 
     def on_click_fast_reverse(self, e):
-        if self.player.playbackState() != QtMultimedia.QMediaPlayer.StoppedState:
+        if self.player.state() != QtMultimedia.QMediaPlayer.StoppedState:
             self.player.setPosition(self.player.position()-1000)
+
+    def on_slider_move(self, position):
+        self.player.setPosition(position)
 
     def on_click_trim_start(self, e):
         self.trim_beginning = self.player.position()
@@ -146,9 +154,25 @@ class TrimWidget(QtWidgets.QWidget):
         self.loop.quit()
         self.destroy()
 
+    def state_changed(self, e):
+        if self.player.state() == QtMultimedia.QMediaPlayer.PlayingState:
+            self.play_pause_button.setIcon(self.style().standardIcon(QtWidgets.QStyle.SP_MediaPause))
+        else:
+            self.play_pause_button.setIcon(self.style().standardIcon(QtWidgets.QStyle.SP_MediaPlay))
+
+    def position_changed(self, position):
+        self.slider.setValue(position)
+
+    def duration_changed(self, length):
+        self.slider.setRange(0, length)
+
 
 if __name__ == '__main__':
+    videos = [sys.argv[1]]
+    print("video " + videos[0])
     app = QtWidgets.QApplication()
-    window = TrimWidget(sys.argv[1:])
+    window = TrimWidget(videos)
+    window.resize(640, 640)
+    window.show()
     app.exec_()
 
