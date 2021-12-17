3373b2543ede89d757756afbf2c3144f6cfd6a41
Louis Adamian <louis@adamian.net>
2021-12-17 13:01:50 -0500

    removes checkbox and model from MainWidget

--- dlc_generic_analysis/dlc_generic_analysis.py
+++ dlc_generic_analysis/dlc_generic_analysis.py
@@ -5,13 +5,11 @@ from abc import abstractmethod
 
 
 class MainWidget(QtWidgets.QWidget):
-    def __init__(self, model_path):
+    def __init__(self):
         super(MainWidget, self).__init__()
         self.setLayout(QtWidgets.QGridLayout())
         self.title = QtWidgets.QLabel("")
         self.layout().addWidget(self.title, alignment=Qt.AlignHCenter)
-        self.dlc_model_path = model_path
-        self.layout().addWidget(self.crop_checkbox)
         self.analyze_button = QtWidgets.QPushButton("Analyze Videos")
         self.analyze_button.clicked.connect(self.on_click_analyze)
         self.layout().addWidget(self.analyze_button)
