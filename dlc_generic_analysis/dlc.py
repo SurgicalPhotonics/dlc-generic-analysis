5e624c90828445c8f47bae1932a1938896115339
Louis Adamian <louis@adamian.net>
2022-01-11 19:41:32 -0500

    updates filtering to ignore data below .85 confidence

--- dlc_generic_analysis/dlc.py
+++ dlc_generic_analysis/dlc.py
@@ -4,13 +4,18 @@ from typing import List
 from logging import info
 
 
-def dlc_analyze(config: str, paths: List[str], gputouse: int = -1) -> (List[str], str):
+def dlc_analyze(config: str, paths: List[str], gputouse: int = 0, filter: bool = True, min_confidence: float = .85) -> (
+List[str], str):
     """
     DLC analyze videos or a batch of videos with the same file extension. will use the 0th paths extension for encoding
     :param config: the directory containing the config.yaml file
     :param paths: the paths to the videos to be analyzed
     :param gputouse: the CUDA capable Nvidia GPU to use for inference. If not set GPU inference will be disabled
-    :return: h5, model_slug: h5: a list of the h5 paths from the analysis, model_slug: the name of then DLC model in the h5 file
+    :param filter: if set true the data will be filtered
+    :param min_confidence: value between 0 and 1 the minimum confidence for data when filtering. any data below
+     min_confidence will be discarded
+    :return: h5, model_slug: h5: a list of the h5 paths from the analysis, model_slug: the name of then DLC model in the
+     h5 file
     """
     cfg = os.path.join(config, "config.yaml")
     _, vidtype = os.path.splitext(paths[0])
@@ -35,13 +40,13 @@ def dlc_analyze(config: str, paths: List[str], gputouse: int = -1) -> (List[str]
             TFGPUinference=False,
         )
     h5 = []
-
-    deeplabcut.filterpredictions(cfg, paths, videotype=vidtype)
-    for path in paths:
-        h5.append(
-            os.path.join(
-                dest, os.path.splitext(os.path.split(path)[1])[0] + model_slug + "_filtered" + ".h5"
+    if filter:
+        deeplabcut.filterpredictions(cfg, paths, videotype=vidtype, filtertype="arima", p_bound=min_confidence)
+        for path in paths:
+            h5.append(
+                os.path.join(
+                    dest, os.path.splitext(os.path.split(path)[1])[0] + model_slug + "_filtered" + ".h5"
+                )
             )
-        )
     info("analysis done")
     return h5, model_slug
