2a69436f6df7b9405a1794b5eea7ba00abca90b4
Louis Adamian <louis@adamian.net>
2022-01-12 13:49:47 -0500

    adds return to threshold_confidence

--- dlc_generic_analysis/filter.py
+++ dlc_generic_analysis/filter.py
@@ -6,17 +6,20 @@ from pandas import DataFrame
 from logging import info
 
 
-def threshold_confidence(h5s: List[str], min_likelihood: float, save_csv=False):
+def threshold_confidence(h5s: List[str], min_likelihood: float, save_csv=False) -> List[str]:
     """
     Thresholds the dlc analysis for a minimum likelihood values less than min_likelihood will be changed to np.nan
     :param h5s: the paths to the h5 files
     :param min_likelihood: the likelihood cutoff all points
     :param save_csv: set true to save a csv file in addition to the hdf5
+    :return: a list to the path of thresholded hdf5s
     """
+    ret_h5s = []
     for h5_path in h5s:
         if not os.path.isfile(h5_path):
             raise FileNotFoundError
         out_path = os.path.splitext(h5_path)[0] + "_threshold.h5"
+        ret_h5s.append(out_path)
         if not os.path.isfile(out_path):
             df = pandas.read_hdf(h5_path)
             assert type(df) == DataFrame
@@ -28,6 +31,7 @@ def threshold_confidence(h5s: List[str], min_likelihood: float, save_csv=False):
                 df.to_csv(out_path.replace(".h5", ".csv"), mode="w")
         else:
             info(f"{h5_path} was already thresholded, skipping")
+    return ret_h5s
 
 
 def __threshold_bp(df: DataFrame, dlc_scorer, cutoff: float, bps: List[str]):
