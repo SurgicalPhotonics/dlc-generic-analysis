48e7da4018b6df71509dc5eafe430fd169ed44da
Louis Adamian <louis@adamian.net>
2022-01-20 16:48:53 -0500

    adds test for dlc_generic_analysis MainWidget

---
+++ dlc_generic_analysis/_tests/test_dlc_generic_analysis.py
@@ -0,0 +1,28 @@
+from dlc_generic_analysis import MainWidget
+
+
+class TestMainWidget(MainWidget):
+    def __init__(self, title: str):
+        super(TestMainWidget, self).__init__(title=title)
+
+    def on_click_analyze(self):
+        pass
+
+    def on_click_view(self):
+        pass
+
+    def on_click_trim(self):
+        super(TestMainWidget, self).on_click_trim()
+
+
+def test_main_widget(qtbot):
+    tmw = TestMainWidget("test")
+    qtbot.addWidget(tmw)
+    tmw.on_click_analyze()
+    tmw.on_click_view()
+
+
+def test_trimmer(qtbot):
+    tmw = TestMainWidget("test")
+    qtbot.addWidget(tmw)
+    tmw.on_click_trim()
